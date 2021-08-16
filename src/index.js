9309a9314242667536a06161ccf0728b02a64157
Vladislava96 <vladislava96.96@mail.ru>
2021-08-16 19:35:01 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) { // -980
+
+
+  let arrN = String(n).split(''); // [-, 9, 8, 0]
+
+  if (arrN[0] === '-') arrN.shift(); //[9, 8, 0] 
+
+  let rev = arrN.reverse(); // [0, 8, 9]
+
+  if (rev[0] === '0') rev.shift(); // [8, 9]
+
+  let str = rev.join(''); // 89
+
+  return str;
 }
